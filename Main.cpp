0e12bf286cfd25c17ed7ad28cdd57b26064ef9e2
Yeou Chiou <yeouchiou@gmail.com>
2019-05-21 17:45:43 -0700

    initial commit

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include "Board.h"
+#include "Player.h"
+#include "Game.h"
+#include "Side.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+}
---
+++ Side.h
@@ -0,0 +1,10 @@
+#ifndef SIDE_H
+#define SID_H
+
+enum Side { NORTH, SOUTH };
+const int NSIDES = 2;
+const int POT = 0;
+
+
+
+#endif
