2346a41dcef2c5861026905817f1d9ddc6799606
Yeou Chiou <yeouchiou@gmail.com>
2019-05-23 15:11:22 -0700

    added all public interfaces

--- Player.h
+++ Player.h
@@ -1,4 +1,22 @@
-#ifndef PLAYER_H
+﻿#ifndef PLAYER_H
 #define PLAYER_H
-
+#include <string>
+class Player
+{
+public:
+	Player(std::string name);
+	// Create a Player with the indicated name.
+	std::string name() const;
+	// Return the name of the player.
+	virtual bool isInteractive() const;
+	// Return false if the player is a computer player.
+	// Return true if the player is human.Most kinds of players will be computer players.
+	virtual int chooseMove(const Board& b, Side s) const = 0;
+	// Every concrete class derived from this class must implement this function 
+	// so that if the player were to be playing side s and had to make a move given board b,
+	// the function returns the move the player would choose.
+	// If no move is possible, return −1.
+	virtual ~Player();
+	// Since this class is designed as a base class, it should have a virtual destructor.
+};
 #endif
