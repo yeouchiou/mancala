9b4c4f6f02f3e9299f96aa1e83aeabea0004e70b
Yeou Chiou <yeouchiou@gmail.com>
2019-05-23 15:29:21 -0700

    added stub functions to Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -1,1 +1,35 @@
 #include "Board.h"
+
+Board::Board(int nHoles, int nInitialBeansPerHole)
+{
+
+}
+
+int Board::holes() const
+{
+	return -999;
+}
+
+int Board::beans(Side s, int hole) const
+{
+	return -999;
+}
+int Board::beansInPlay(Side s) const
+{
+	return -999;
+}
+
+bool Board::sow(Side s, int hole, Side& endSide, int& endHole)
+{
+	return false;
+}
+
+bool Board::moveToPot(Side s, int hole, Side potOwner)
+{
+	return false;
+}
+
+bool Board::setBeans(Side s, int hole, int beans)
+{
+	return false;
+}
