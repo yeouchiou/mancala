400ba0f5086bd5d1e1813546456ec07e0f9d9c2c
Yeou Chiou <yeouchiou@gmail.com>
2019-05-23 15:21:20 -0700

    added stub functions

--- Game.cpp
+++ Game.cpp
@@ -1,1 +1,31 @@
 #include "Game.h"
+
+Game::Game(const Board& b, Player* south, Player* north)
+{
+
+}
+
+void Game::display() const
+{
+	return;
+}
+
+void Game::status(bool& over, bool& hasWinner, Side& winner) const
+{
+	return;
+}
+
+bool Game::move()
+{
+	return;
+}
+
+void Game::play()
+{
+	return;
+}
+
+int Game::beans(Side s, int hole) const
+{
+	return -999;
+}
